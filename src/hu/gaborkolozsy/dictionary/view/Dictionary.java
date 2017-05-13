19fa9626ac9c53c0988b1aa665105cd43ee13719
Gábor Kolozsy <gabor.kolozsy.development@gmail.com>
2017-05-13 09:26:22 +0200

    Update Dictionary.java

--- src/hu/gaborkolozsy/dictionary/view/Dictionary.java
+++ src/hu/gaborkolozsy/dictionary/view/Dictionary.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package hu.gaborkolozsy.dictionary.view;
 
 import hu.gaborkolozsy.dictionary.controller.DictionaryService;
